4426c2d21b2cfe283a4ca83662916cd0f9e15a78
Johnlocke0347 <johnlocke9237@gmail.com>
2026-02-09 19:29:49 +0500

    adding aggregator.py

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python3 -m http.server 8080 --bind 0.0.0.0 &
 
-python3 /app/test_miner.py || echo "Miner script crashed, keeping container alive for debugging"
-
-wait
+python3 /app/hub/aggregator.py &
+python3 /app/test_miner.py
