296871979f72356f1776f93f025566ecb94dc49f
Johnlocke0347 <johnlocke9237@gmail.com>
2026-02-09 18:11:50 +0500

    implement mock prover response in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,5 @@
 while true; do
   {
     echo -e "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 61\r\n\r\n{\"status\":\"active\",\"f1\":0.87,\"proof\":\"0x1a2b3c4d5e6f7890\"}"
-  } | nc -lkp 8080
+  } | nc -lkp 8080 -q 1
 done
