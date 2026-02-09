3b73f8158b71c826455de88c892b53c31cce1254
Johnlocke0347 <johnlocke9237@gmail.com>
2026-02-09 18:21:44 +0500

    using standard pipe

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 while true; do
-  {
-    echo -e "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 61\r\n\r\n{\"status\":\"active\",\"f1\":0.87,\"proof\":\"0x1a2b3c4d5e6f7890\"}"
-  } | nc -lp 8080 -q 1
+  echo -e "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 61\r\n\r\n{\"status\":\"active\",\"f1\":0.87,\"proof\":\"0x1a2b3c4d5e6f7890\"}" | nc -l -p 8080 -N
 done
